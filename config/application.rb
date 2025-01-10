7d5232f386d4439187727f3cac4daf0ec161c333
hinh2003 <vanhinh2003@gmail.com>
2025-01-10 10:28:09 +0700

    setup backend Authentication

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,9 @@ module Socail
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
